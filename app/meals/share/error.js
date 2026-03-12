4922e7b8e3c33a90256c3f26c667519ccbd1118b
Samarjeet Sandeep Bhosale <samarjeet2456@gmail.com>
2026-03-13 00:07:33 +0530

    Initial commit

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Error() {
+    return(
+        <main classname="error">
+            <h1>An Error occured!</h1>
+            <p>Failed to create meal.</p>
+        </main>
+    )
+}
